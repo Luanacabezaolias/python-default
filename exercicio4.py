57f24d43fa76d4122725d713cd10b7726e9abc84
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-11 11:43:55 -0300

    exercicios

--- exercicio4.py
+++ exercicio4.py
@@ -4,4 +4,11 @@ O parâmetro corpo deve ter um valor padrão de uma string vazia. A função dev
 imprimir as informações formatadas. Inclua uma docstring que explique brevemente 
 o propósito da função.'''
 
-def enviar_email()
+def enviar_email(destinatario, assunto="Sem assunto", corpo=''):
+    print(f'Destinatario: {destinatario}')
+    print(f'Assunto: {assunto}')
+    print(f'Corpo: {corpo}')
+
+enviar_email("lu.linda@gmail.com")
+enviar_email("lu.linda@gmail.com, urgente")
+enviar_email("lu.linda@gmail.com" corpo="conteudo do email")
